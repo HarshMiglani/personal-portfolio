8588351de5dbbe7f0fd9df8c910baba425b677bb
Harsh <harsh.miglani010@gmail.com>
2023-07-25 15:12:14 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className = 'App'>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/personal-portfolio" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/education" element={<Education />} />
         <Route path="/experience" element={<Experience />} />
